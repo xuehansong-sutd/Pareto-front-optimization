af981130981afe4f3aebeb934b3acacaf8b5a3f5
xuehansong-sutd <152177855+xuehansong-sutd@users.noreply.github.com>
2023-12-03 17:16:00 +0800

    Deleted some repeated files

--- HL_optimization_run1 Min Cost/Unique Search Parameters/Unique_Search_Parameters.m
+++
@@ -1,9 +0,0 @@
-clear
-clc
-close all
-
-Parameter = load('Layer_EffTEffB_Cost.txt');
-
-unique_parameter = unique(Parameter,'rows');
-
-writematrix(unique_parameter,'unique_parameter.txt','Delimiter','\t');
